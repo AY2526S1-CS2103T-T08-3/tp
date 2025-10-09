52333fdad884bd0bdf3f26d31cf8b21844f29834
Jordan Low <jordanlow.jinyi@gmail.com>
2025-10-09 20:51:50 +0800

    Fix logical merge conflicts

--- src/main/java/seedu/address/logic/commands/AssignCommand.java
+++ src/main/java/seedu/address/logic/commands/AssignCommand.java
@@ -62,7 +62,8 @@ public class AssignCommand extends Command {
         updatedCategories.add(categoryObject);
 
         Person editedPerson = new Person(personToEdit.getName(), personToEdit.getPhone(), personToEdit.getEmail(),
-                personToEdit.getAddress(), updatedCategories, personToEdit.getTags());
+                personToEdit.getDept(), personToEdit.getRole(), personToEdit.getTeam(), personToEdit.getSkills(), 
+                updatedCategories, personToEdit.getTags());
 
         model.setPerson(personToEdit, editedPerson);
         model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
