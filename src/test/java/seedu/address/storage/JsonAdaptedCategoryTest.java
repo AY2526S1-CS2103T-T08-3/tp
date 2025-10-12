99ff524c36dc82c1e7c5ad6d5b658568ed976384
xavychin <xavychin@gmail.com>
2025-10-13 00:10:31 +0800

    Fix checkstyle error

--- src/test/java/seedu/address/storage/JsonAdaptedCategoryTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedCategoryTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.tag.Category;
 
 public class JsonAdaptedCategoryTest {
